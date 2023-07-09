1ca38c1175752fe8e3a48f012e391cee538f7d5c
llcawc <hostmastername@gmail.com>
2023-07-09 15:51:57 +0300

    revision

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,13 @@
 // main.js
 
-import { name, version } from '../../../package.json'
+import { name, version, date } from '../../../package.json'
 import data from './data.js'
 
 if (document.querySelector('.year')) {
   document.querySelector('.year').innerHTML = new Date().getFullYear()
 }
 
-console.log(`Package name: "${name}". Version: ${version}`, '\ndata.js:JSON.stringify...', JSON.stringify(data), '\njs script is running ...', data.text)
+console.log(
+  `Package name: "${name}". Version: ${version}. Date: ${date}`,
+  '\ndata.js:JSON.stringify...', JSON.stringify(data),
+  '\njs script is running ...', data.text)
